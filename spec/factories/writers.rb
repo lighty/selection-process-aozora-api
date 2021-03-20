301382ca84c6501c3ae29b907da6b08974de1215
Hikaru Watanabe <hikalin8686@gmail.com>
2021-03-21 02:54:35 +0900

    rspecの環境作成 (#4)

---
+++ spec/factories/writers.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: writers
+#
+#  id            :bigint           not null, primary key
+#  born_at       :date
+#  dead_at       :date
+#  name          :string           default(""), not null
+#  pronunciation :string           default(""), not null
+#  romaji        :string           default(""), not null
+#  summary       :string           default(""), not null
+#  wikipedia_url :string           default(""), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+FactoryBot.define do
+  factory :writer do
+    born_at { '1867-02-09' }
+    dead_at { '1916-12-09' }
+    name { '夏目 漱石' }
+    pronunciation { 'なつめ そうせき' }
+    summary { '慶応3年1月5日（新暦2月9日）江戸牛込馬場下横町に生まれる。' }
+    wikipedia_url { 'http://ja.wikipedia.org/' }
+  end
+end
