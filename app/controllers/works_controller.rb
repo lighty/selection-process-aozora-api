30e7fd84fdf8ccae2ee93e933f467c28ff49215f
lighty <hikalin8686@gmail.com>
2021-03-21 12:05:25 +0900

    limitないと1万件以上取れちゃうのでデフォで入れておく

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,6 @@
 class WorksController < ApplicationController
   def index
-    works = Work.where('name like ?', "%#{params[:q]}%")
+    works = Work.where('name like ?', "%#{params[:q]}%").limit(100)
     render json: works
   end
 end
